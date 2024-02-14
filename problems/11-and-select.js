02b04dc1c9b9f11b1ea6d204b11e1a2411a84b9f
DamonRyon <damon.ryon@gmail.com>
2024-02-13 21:40:23 -0700

    prob11

--- problems/11-and-select.js
+++ problems/11-and-select.js
@@ -25,13 +25,44 @@ let startsWithA = function (s) {
 }
 console.log(andSelect(['ants', 'APPLES', 'ART', 'BACON', 'arm'], isUpperCase,  startsWithA));
 // [ 'APPLES', 'ART' ]
-
+Write a function `andSelect` that accepts an array and two callbacks. The function
+should return a new array containing elements of the original array that result in
+true when passed into both callbacks.
 *******************************************************************************/
 
 function andSelect(array, cb1, cb2) {
-  // Your code here 
+  let result = []
+  for (let i = 0; i < array.length; i++){
+    let test = array[i];
+    if (cb1(test) === true){
+      if (cb2(test) === true){
+        result.push(test)
+      }
+    }
+  }
+  return result;
 }
 
+let isEven = function (n) {
+  return n % 2 === 0;
+};
+
+let isPositive = function (n) {
+  return n > 0;
+};
+
+console.log(andSelect([-3, 8, 7, 11, 6, 12, -4], isEven, isPositive));
+// [ 8, 6, 12 ]
+
+let isUpperCase = function (s) {
+  return s === s.toUpperCase();
+};
+
+let startsWithA = function (s) {
+  return s[0].toUpperCase() === 'A';
+}
+console.log(andSelect(['ants', 'APPLES', 'ART', 'BACON', 'arm'], isUpperCase,  startsWithA));
+// [ 'APPLES', 'ART' ]
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = andSelect;
